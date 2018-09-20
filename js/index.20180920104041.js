36fafa9e62ef96b84a13b3cb8ccb645501f49382
zVivien <1084472440@qq.com>
2018-09-20 11:41:34 +0800

    1

--- js/index.20180920104041.js
+++
@@ -1,9 +0,0 @@
-(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
-$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
-
-
-$(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/ping-mu-kuai-zhao-2018-09-20-shang-wu9.42.53-400.jpg' : 'images/ping-mu-kuai-zhao-2018-09-20-shang-wu9.42.53-200.jpg');};
-if(!window.HTMLPictureElement){r();}
-
-});
